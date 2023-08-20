c6db2c3b652aea28a16e88e284650532e155e665
Jakub Wawrzyczek <jakubwawrzyczek.contact@gmail.com>
2023-08-20 22:25:07 +0200

    watermarking const image and showing it

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('Hello world')
+from PIL import Image, ImageDraw, ImageFont
+
+img_path = 'img/image.jpg'
+font_path = '/System/Library/Fonts/Supplemental/Arial.ttf'
+watermark_pos = (0, 0)
+watermark_text = 'sample text'
+color = (255, 255, 255)
+
+font = ImageFont.truetype(font_path, 100)
+img = Image.open(img_path)
+
+drawable_img = ImageDraw.Draw(img)
+drawable_img.text(watermark_pos, watermark_text, font=font, fill=color)
+
+img.show()
