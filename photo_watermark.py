432a52a85c017867b2d4220e8135f1aa3bc20e90
Jakub Wawrzyczek <jakubwawrzyczek.contact@gmail.com>
2023-08-22 16:47:03 +0200

    Added Center-Aligned Image Watermarks

--- photo_watermark.py
+++ photo_watermark.py
@@ -1,3 +1,12 @@
+from PIL import Image, ImageDraw, ImageFont
+
+
 class PhotoWatermark:
-    def add_photo_watermark(self, path, watermark_image, watermark_size):
-        pass
+    def add_photo_watermark(self, path, watermark_image_path, size_option):
+        img = Image.open(path)
+        watermark_image = Image.open(watermark_image_path)
+        watermark_image = watermark_image.resize((int(watermark_image.width / (size_option / 3)), int(watermark_image.height / (size_option / 3))))
+
+        img = img.copy()
+        img.paste(watermark_image, (int((img.width // 2) - watermark_image.width // 2), int((img.height // 2) - watermark_image.height // 2)))
+        return img
--- text_watermark.py
+++ text_watermark.py
@@ -4,14 +4,14 @@ TEXT_COLOR = (255, 255, 255)
 
 
 class TextWatermark:
-    def add_text_watermark(self, path, text, watermark_size):
+    def add_text_watermark(self, path, text, size_option):
         img = Image.open(path)
-        size = img.size
+        img_size = img.size
 
-        font = ImageFont.truetype('/System/Library/Fonts/Supplemental/Arial.ttf', int(size[0] / watermark_size))
-        watermark_pos = (0, int(size[1] / 2))
+        font = ImageFont.truetype('/System/Library/Fonts/Supplemental/Arial.ttf', int(img_size[0] / size_option))
+        watermark_pos = (0, int(img_size[1] / 2))
 
         drawable_img = ImageDraw.Draw(img)
-        drawable_img.text(watermark_pos, (text + ' ') * watermark_size * 2, font=font, fill=TEXT_COLOR)
+        drawable_img.text(watermark_pos, (text + ' ') * size_option * 2, font=font, fill=TEXT_COLOR)
 
         return img
