3ae15126bce712c6401b6d2d739a48e6cbf9208f
Jakub Wawrzyczek <jakubwawrzyczek.contact@gmail.com>
2023-08-23 14:10:25 +0200

    added rendering window, connected watermark function to add watermark button

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from tkinter import *
 import photo_watermark
 import text_watermark
 import tui
@@ -6,21 +7,28 @@ tui = tui.Tui()
 photo_watermark = photo_watermark.PhotoWatermark()
 text_watermark = text_watermark.TextWatermark()
 
-
 def watermark():
     watermark_type = tui.get_watermark_type()
 
     if watermark_type == 'TEXT':
-        return text_watermark.add_text_watermark(tui.get_image_path(),
+        text_watermark.add_text_watermark(tui.get_image_path(),
                                                  tui.get_watermark_text(),
-                                                 tui.get_watermark_size())
+                                                 tui.get_watermark_size()).show()
     elif watermark_type == 'IMAGE':
-        return photo_watermark.add_photo_watermark(tui.get_image_path(),
+        photo_watermark.add_photo_watermark(tui.get_image_path(),
                                                    tui.get_watermark_image(),
-                                                   tui.get_watermark_size())
+                                                   tui.get_watermark_size()).show()
     else:
         print('Incorrect type')
 
 
-img = watermark()
-img.show()
+# window config
+window = Tk()
+window.title('Watermark App')
+
+# buttons
+btn_add_watermark = Button(text='Add Watermark', command=watermark)
+btn_add_watermark.grid(row=0, column=0)
+
+window.mainloop()
+
